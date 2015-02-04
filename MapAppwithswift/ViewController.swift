290d4792291b74f6565d00230a1c04f4defa39be
MikailaAkeredolu <makerofappsdotcom@gmail.com>
2015-02-04 07:51:17 -0500

    update code to display user addy

--- MapAppwithswift/ViewController.swift
+++ MapAppwithswift/ViewController.swift
@@ -13,6 +13,22 @@ import CoreLocation
 //add map delegate
 class ViewController: UIViewController,CLLocationManagerDelegate {
     
+    //outlets
+    
+    @IBOutlet weak var latitude: UILabel!
+    
+    @IBOutlet weak var longitude: UILabel!
+    
+    @IBOutlet weak var heading: UILabel!
+    
+    @IBOutlet weak var speed: UILabel!
+    
+    @IBOutlet weak var altitude: UILabel!
+    
+    @IBOutlet weak var address: UILabel!
+    
+    
+    
     //create a variable of type cllocation manager
     var manager: CLLocationManager!
     
@@ -33,14 +49,54 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
     
     //did update locations function
     
-    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!)
+    { //locations is an array
         
-        var userLocation:CLLocation = locations[0] as CLLocation
+        var userLocation:CLLocation = locations[0] as CLLocation //display first location
         
-        println(userLocation)
+        //get latitude and use string interpolation to pass to text label
         
+        latitude.text = "\(userLocation.coordinate.latitude)"
+        
+        longitude.text = "\(userLocation.coordinate.longitude)"
+        
+        heading.text = "\(userLocation.course)" //course property gives you direction
+        
+        speed.text = "\(userLocation.speed)"
+        
+        altitude.text = "\(userLocation.altitude)"
+        
+        
+        //get address with geolocation
+        CLGeocoder().reverseGeocodeLocation(userLocation, completionHandler:{( placemarks, error) in
+            
+            if error != nil{ println(error)}
+            else{
+               
+                //create variable to access placemark
+                let p = CLPlacemark(placemark: placemarks?[0] as CLPlacemark)
+                
+                var subThoroughfare:String
+                
+                if ((p.subThoroughfare) != nil){ //if it exist
+                    
+                    subThoroughfare = p.subThoroughfare
+                    
+                }else{
+                   subThoroughfare = "" //if not make it empty string
+                }
+                
+                
+                self.address.text = "\(subThoroughfare)  \(p.thoroughfare) \n  \(p.subLocality) \n \(p.subAdministrativeArea) \n \(p.postalCode) \(p.country)"
+                
+                
+            }
+            
+            
+        })
         
     }
+            
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
