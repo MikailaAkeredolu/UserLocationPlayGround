83d2ccf80278e1978dbb25485bf49c652d928fb7
MikailaAkeredolu <makerofappsdotcom@gmail.com>
2015-02-04 06:39:00 -0500

    first commit

--- MapAppwithswift/ViewController.swift
+++ MapAppwithswift/ViewController.swift
@@ -4,15 +4,42 @@
 //
 //  Created by Mikaila Akeredolu on 2/1/15.
 //  Copyright (c) 2015 MakerOfAppsDotCom. All rights reserved.
-//
+
+//App to know your lat & long plus closest address
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+//add map delegate
+class ViewController: UIViewController,CLLocationManagerDelegate {
+    
+    //create a variable of type cllocation manager
+    var manager: CLLocationManager!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization() //ask user to share location
+        manager.startUpdatingLocation()
+        
+        
+        
+    }
+    
+    //did update locations function
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        
+        var userLocation:CLLocation = locations[0] as CLLocation
+        
+        println(userLocation)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
